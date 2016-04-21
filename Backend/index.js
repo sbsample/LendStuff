87c7b4c79ef78661b42a0612b0b8453dc55b9eb0
Ellis Marte <ellismarte@punch-agency.com>
2016-04-20 20:39:30 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,27 @@
 "use strict";
+
+/**
+ * Module dependencies.
+ */
 const express = require('express');
 const app     = express();
 const port    = 3000;
 
+/**
+ * Imported routes.
+ */
 const users   = require('./app/controllers/users/user-ctrl.js')(express.Router());
 // const   = require('.app/controllers/')
 // const   = require('.app/controllers/')
 
+/**
+ * Mounted routes. 
+ */
 app.use('/users', users);
 
+/**
+ * Server listening on variable port. 
+ */
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 })
