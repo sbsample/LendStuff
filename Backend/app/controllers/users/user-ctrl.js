ff26fcc46be8675ad4e1cf222b862028f4d4c6e0
Ellis Marte <ellismarte@punch-agency.com>
2016-04-20 20:43:15 -0700

    Update user-ctrl.js

--- Backend/app/controllers/users/user-ctrl.js
+++ Backend/app/controllers/users/user-ctrl.js
@@ -1,14 +1,22 @@
 "use strict";
 
+/**
+ * User routes.
+ */
+ 
 module.exports = (router) => {
 
 /**
  * Create route accepts the following parameters:
  * email, username, password
+ * Route is used for creating a new account. 
  */
   router.post('/create', (req, res) => {
     
   });
 
+/**
+ * Return router so that it can be used by express. 
+ */
   return router;
 }
