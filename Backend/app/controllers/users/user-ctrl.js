e64a9f12b95dc1cd796837748c0fea2813c04a68
Staunton Sample <stauntonsample@gmail.com>
2016-04-26 12:06:54 -0700

    add user put, delete routes

--- Backend/app/controllers/users/user-ctrl.js
+++ Backend/app/controllers/users/user-ctrl.js
@@ -7,14 +7,22 @@
 module.exports = (router) => {
 
 router.get('/new', function(req, res){
-  res.send('hello world -- from Docker World')
+    res.send('hello world -- from Docker World')
 })
 /**
  * Create route accepts the following parameters:
  * email, username, password
  * Route is used for creating a new account.
  */
-  router.post('/create', (req, res) => {
+  router.post('/create', function(req, res) {
+
+  });
+
+  router.put('/:id',function(req, res){
+
+  });
+
+  router.delete('/:id', function(req, res){
 
   });
 
