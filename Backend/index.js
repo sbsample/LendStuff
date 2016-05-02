9dca79a73eef1c882fe50b152a2d38595f12acdb
Ellis Marte <elliscmarte@gmail.com>
2016-05-01 20:27:13 -0700

    work on clustering feature

--- Backend/index.js
+++ Backend/index.js
@@ -25,3 +25,21 @@ app.use('/users', users);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 })
+
+// const cluster = require('cluster');
+// const numCPUs = require('os').cpus().length;
+//
+// if (cluster.isMaster) {
+//   console.log('Fork %s worker(s) from master', numCPUs);
+//   for (var i = 0; i < numCPUs; i++){
+//     cluster.fork();
+//   }
+//   cluster.on('online', function(worker){
+//     console.log('Worker is running on %s pid', worker.process.pid);
+//   });
+//   else if (cluster.isWorker){
+//     var port = 3000;
+//     console.log('Worker (%s) is now listening to http://localhost:%s', cluster.worker.process.pid, port);
+//     app.listen(port);
+//   }
+// }
